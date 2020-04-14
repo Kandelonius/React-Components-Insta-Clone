fbe5a681c8789a11f8b1a5f973d03122ffc2a998
Shane Kaestner <shane.kaestner@yahoo.com>
2020-04-14 14:33:56 -0700

    I have the post displaying on the page allong with the search bar. I don't have any comments showing yet on the posts.

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -5,15 +5,16 @@ import Post from "./Post";
 import "./Posts.css";
 import dummyData from "../../../src/dummy-data.js"
 // import data 
-
+// { dummyData }
 const PostsPage = () => {
   console.log(dummyData);
   // set up state for your data
-  debugger
+  const [post, setPost] = useState(dummyData);
+  // debugger
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
-
+      {dummyData.map(post => <Post post={post}/>)}
     </div>
   );
 };
